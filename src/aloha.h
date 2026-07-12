81883b9456bd61ecf3743af044fa143d65405d86
iestyn129 <iestyn129msm@gmail.com>
2026-07-12 01:12:51 +0100

    fuck you lua you were the cause of all my problems

--- src/aloha.h
+++ src/aloha.h
@@ -15,6 +15,8 @@ extern "C" void spawn_birds_cue(long long int, long long int, char*);
 extern "C" void spawn_talk_cue01(long long int);
 extern "C" void spawn_wiper_cue01(long long int);
 extern "C" void spawn_wiper_cue02(long long int);
+extern "C" void spawn_flydisc_cue(long long int, long long int, unsigned int, unsigned int, int, char);
+extern "C" void spawn_rope_cue(long long int, long long int, int, char);
 
 extern "C" long long int sub_7100137990();
 extern "C" long long int set_anim(long long int, long long int, int ticks, int alsoTicks, int maybeAnID); // ???
--- src/main.cpp
+++ src/main.cpp
@@ -9,8 +9,15 @@
     if (is_thread_stopping(a2)) \
         return;
 
+#define check_threadc(a2) \
+    if (is_thread_stopping(a2)) \
+        return sub_7100514FD0(a2);;
+
 #define restb(beats) rest_ticks(a2, 480 * beats); \
     check_thread(a2);
+
+#define restbc(beats) rest_ticks(a2, 480 * beats); \
+    check_threadc(a2);
 
 #define scene_change_r(scene) \
     do { \
@@ -27,6 +34,38 @@
         unk_thread_check(a2, v1); \
         check_thread(a2); \
     } while (0);
+
+#define cue_scene_change(scene) \
+    do { \
+        v1 = sub_7100514FB0(a2); \
+        change_cue_scene(a1, a2, scene); \
+        unk_thread_check(a2, v1); \
+        check_threadc(a2); \
+    } while (0);
+
+#define set_criteria(criteria) \
+    set_marking_criteria(a1, const_cast<char*>(criteria))
+
+#define parasol_cue(ticks, a4, a5, a6) \
+    spawn_parasol_cue(reinterpret_cast<s64*>(a1)[7313], a2, ticks, a4, a5, a6)
+
+#define birds_cue(cue) \
+    spawn_birds_cue(reinterpret_cast<s64*>(a1)[7265], a2, const_cast<char*>(cue))
+
+#define talk_cue01() \
+    spawn_talk_cue01(reinterpret_cast<s64*>(a1)[7340])
+
+#define wiper_cue01() \
+    spawn_wiper_cue01(reinterpret_cast<s64*>(a1)[7349])
+
+#define wiper_cue02() \
+    spawn_wiper_cue02(reinterpret_cast<s64*>(a1)[7349])
+
+#define flydisc_cue(a3, ticks, a5, a6) \
+    spawn_flydisc_cue(reinterpret_cast<s64*>(a1)[7286], a2, a3, ticks, a5, a6)
+
+#define rope_cue(ticks, a4) \
+    spawn_rope_cue(reinterpret_cast<s64*>(a1)[7328], a2, ticks, a4)
 
 
 HkTrampoline initHook = [](TrampolineStatic(), u64* a1) -> void {
@@ -82,84 +121,170 @@ void remix20Control(s64 a1, s64 a2) {
     restb(8);
 
     scene_change_r(22);
-    restb(8);
+    restb(16);
 }
 
 
 void remix20Anim(s64 a1, s64 a2) {
     // runLuaChart(a1, a2, ANIM_SCRIPT);
-    restb(20);
+    restb(34);
+
+    set_anim(a1, a2, 0, 480, 0);
+    restb(26);
+
+    stop_anim(a1);
+    restb(6);
 
     set_anim(a1, a2, 0, 480, 0);
 }
 
 
 HkTrampoline remix20Cues00Hook = [](TrampolineStatic(), s64 a1, s64 a2) -> s64 {
-    runLuaChart(a1, a2, CUES00_SCRIPT);
+    //runLuaChart(a1, a2, CUES00_SCRIPT);
+    s64 v1;
+
+    restbc(16);
+
+    cue_scene_change(17);
+    set_criteria("basic");
+    restbc(1);
+
+    parasol_cue(480, 1, 0, 0);
+    restbc(8);
+    parasol_cue(480, 0, 0, 0);
 
     return sub_7100514FD0(a2);
 };
 
 
 HkTrampoline remix20Cues01Hook = [](TrampolineStatic(), s64 a1, s64 a2) -> s64 {
-    runLuaChart(a1, a2, CUES01_SCRIPT);
+    //runLuaChart(a1, a2, CUES01_SCRIPT);
+    s64 v1;
+
+    restbc(34);
+
+    cue_scene_change(1);
+    set_criteria("basic");
+    restbc(1);
+
+    birds_cue("D...U...D...U?..");
 
     return sub_7100514FD0(a2);
 };
 
 
 HkTrampoline remix20Cues02Hook = [](TrampolineStatic(), s64 a1, s64 a2) -> s64 {
-    runLuaChart(a1, a2, CUES02_SCRIPT);
+    //runLuaChart(a1, a2, CUES02_SCRIPT);
+    s64 v1;
+
+    restbc(42);
+
+    cue_scene_change(26);
+    set_criteria("basic");
+    restbc(1);
+
+    talk_cue01();
+    restbc(4);
+    talk_cue01();
+    restbc(4);
 
     return sub_7100514FD0(a2);
 };
 
 
 HkTrampoline remix20Cues03Hook = [](TrampolineStatic(), s64 a1, s64 a2) -> s64 {
-    runLuaChart(a1, a2, CUES03_SCRIPT);
+    //runLuaChart(a1, a2, CUES03_SCRIPT);
+    s64 v1;
+
+    restbc(50);
+
+    cue_scene_change(29);
+    set_criteria("basic");
+    restbc(1);
+
+    wiper_cue01();
+    restbc(4);
+    wiper_cue02();
+    restbc(4);
 
     return sub_7100514FD0(a2);
 };
 
 
 HkTrampoline remix20Cues04Hook = [](TrampolineStatic(), s64 a1, s64 a2) -> s64 {
-    runLuaChart(a1, a2, CUES04_SCRIPT);
+    //runLuaChart(a1, a2, CUES04_SCRIPT);
+    s64 v1;
+
+    restbc(55);
+
+    cue_scene_change(8);
+    set_criteria("must");
+    restbc(1);
+
+    flydisc_cue(0, 7 * 480, -1, 0);
+    restbc(11);
 
     return sub_7100514FD0(a2);
 };
 
 
 HkTrampoline remix20Cues05Hook = [](TrampolineStatic(), s64 a1, s64 a2) -> s64 {
-    runLuaChart(a1, a2, CUES05_SCRIPT);
+    //runLuaChart(a1, a2, CUES05_SCRIPT);
+    s64 v1;
+
+    restbc(65);
+
+    cue_scene_change(22);
+    set_criteria("basic");
+    restbc(1);
+
+    rope_cue(480, 0);
+    restbc(1);
+    rope_cue(480, 0);
+    restbc(1);
+
+    rope_cue(480, 0);
+    restbc(1);
+    rope_cue(480, 0);
+    restbc(1);
+    rope_cue(480, 0);
+    restbc(1);
+    rope_cue(480, 0);
+    restbc(1);
+
+    rope_cue(480, 0);
+    restbc(1);
+    rope_cue(480, 0);
+    restbc(1);
+    rope_cue(480, 0);
+    restbc(1);
+    rope_cue(480, 0);
+    restbc(1);
+
+    rope_cue(480, 0);
+    restbc(1);
+    rope_cue(480, 0);
+    restbc(1);
+    rope_cue(480, 0);
+    restbc(1);
+    rope_cue(480, 1);
+    restbc(5);
 
     return sub_7100514FD0(a2);
 };
 
 
 HkTrampoline remix20Cues06Hook = [](TrampolineStatic(), s64 a1, s64 a2) -> s64 {
-    runLuaChart(a1, a2, CUES06_SCRIPT);
-
-    //rest_ticks(a2, 480 * 58);
-
-    //sub_7100514FB0(a2);
-    //change_cue_scene(a1, a2, 10);
-
-    //set_marking_criteria(a1, (char*)"basic");
-    //spawn_guardian_cue02(*(s64*)(a1 + 58336), a2, 0, 0);
-
-    //rest_ticks(a2, 480 * 8);
-
-    //set_marking_criteria(a1, (char*)"basic");
-    //spawn_guardian_cue02(*(s64*)(a1 + 58336), a2, 0, 0);
-
-    //rest_ticks(a2, 480 * 504);
+    //runLuaChart(a1, a2, CUES06_SCRIPT);
+    s64 v1;
 
     return sub_7100514FD0(a2);
 };
 
 
 HkTrampoline remix20Cues07Hook = [](TrampolineStatic(), s64 a1, s64 a2) -> s64 {
-    runLuaChart(a1, a2, CUES07_SCRIPT);
+    //runLuaChart(a1, a2, CUES07_SCRIPT);
+    s64 v1;
 
     return sub_7100514FD0(a2);
 };
