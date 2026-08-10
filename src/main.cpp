4e59124960c226109c0d22534b2f219ad8d08308
iestyn129 <iestyn129msm@gmail.com>
2026-08-10 13:49:11 +0100

    remove code i dont need

--- src/main.cpp
+++ src/main.cpp
@@ -145,87 +145,9 @@ HkTrampoline remix20EntryHook = [](TrampolineStatic(), StageRemix20* stage, SeqT
     thread->popUnk270();
 };
 
-/*
-void remix20Main(StageRemix20* stage, SeqThread* thread) {
-    chartMain(stage, thread);
-}
-
-
-void remix20Control(StageRemix20* stage, SeqThread* thread) {
-    chartControl(stage, thread);
-}
-
-
-void remix20Anim(StageRemix20* stage, SeqThread* thread) {
-    chartAnim(stage, thread);
-}
-
-
-HkTrampoline remix20InitSubScenesHook = [](TrampolineStatic(), StageRemix20* stage, SeqThread* thread) -> s64 {
-    chartInitSubScenes(stage, thread);
-
-    return thread->popUnk270();
-};
-
-
-HkTrampoline remix20Cues00Hook = [](TrampolineStatic(), StageRemix20* stage, SeqThread* thread) -> s64 {
-    chartCues00(stage, thread);
-
-    return thread->popUnk270();
-};
-
-
-HkTrampoline remix20Cues01Hook = [](TrampolineStatic(), StageRemix20* stage, SeqThread* thread) -> s64 {
-    chartCues01(stage, thread);
-
-    return thread->popUnk270();
-};
-
-
-HkTrampoline remix20Cues02Hook = [](TrampolineStatic(), StageRemix20* stage, SeqThread* thread) -> s64 {
-    chartCues02(stage, thread);
-
-    return thread->popUnk270();
-};
-
-
-HkTrampoline remix20Cues03Hook = [](TrampolineStatic(), StageRemix20* stage, SeqThread* thread) -> s64 {
-    chartCues03(stage, thread);
-
-    return thread->popUnk270();
-};
-
-
-HkTrampoline remix20Cues04Hook = [](TrampolineStatic(), StageRemix20* stage, SeqThread* thread) -> s64 {
-    chartCues04(stage, thread);
-
-    return thread->popUnk270();
-};
-
-
-HkTrampoline remix20Cues05Hook = [](TrampolineStatic(), StageRemix20* stage, SeqThread* thread) -> s64 {
-    chartCues05(stage, thread);
-
-    return thread->popUnk270();
-};
-
-
-HkTrampoline remix20Cues06Hook = [](TrampolineStatic(), StageRemix20* stage, SeqThread* thread) -> s64 {
-    chartCues06(stage, thread);
-
-    return thread->popUnk270();
-};
-
-
-HkTrampoline remix20Cues07Hook = [](TrampolineStatic(), StageRemix20* stage, SeqThread* thread) -> s64 {
-    chartCues07(stage, thread);
-
-    return thread->popUnk270();
-};
-*/
 
 extern "C" void hkMain() {
-    const ptr mainBase = hk::ro::getMainModule()->range().start();
+    //const ptr mainBase = hk::ro::getMainModule()->range().start();
 
     initHook.installAtMainOffset(0x50CBA0);
     mainLoopHook.installAtMainOffset(0x509E10);
@@ -234,65 +156,16 @@ extern "C" void hkMain() {
 
     remix20EntryHook.installAtMainOffset(0x416220);
 
-/*
-    getGraphBacteriaHook.installAtMainOffset(0x1434E0);
-    getGraphBrushHook.installAtMainOffset(0x158A00);
-    getGraphClapHook.installAtMainOffset(0x161D40);
-    getGraphHammerHook.installAtMainOffset(0x1A9920);
-    getGraphMoonHook.installAtMainOffset(0x1D8B60);
-    getGraphRingHook.installAtMainOffset(0x216530);
-    getGraphRopeHook.installAtMainOffset(0x224790);
-
-    hk::hook::a64::assemble<"mov w1, {}">()
-        .arg(initSceneID)
-        .installAtMainOffset(0x4170B8); // FUN_71004d9dc0
-
-    hk::hook::a64::assemble<"mov w2, {}">()
-        .arg(initSceneID)
-        .installAtMainOffset(0x4170DC); // FUN_71004d9ec0
-
-    hk::hook::a64::assemble<"mov w2, {}">()
-        .arg(initSceneID)
-        .installAtMainOffset(0x417118); // sceneChangeInstant
-
-    hk::hook::a64::assemble<"mov w1, {}">()
-        .arg(initRingBubbleState)
-        .installAtMainOffset(0x4170a8); // StageRemix::setRingBubbleState
+    //getGraphBacteriaHook.installAtMainOffset(0x1434E0);
+    //getGraphBrushHook.installAtMainOffset(0x158A00);
+    //getGraphClapHook.installAtMainOffset(0x161D40);
+    //getGraphHammerHook.installAtMainOffset(0x1A9920);
+    //getGraphMoonHook.installAtMainOffset(0x1D8B60);
+    //getGraphRingHook.installAtMainOffset(0x216530);
+    //getGraphRopeHook.installAtMainOffset(0x224790);
 
     // over germ aerobics
-    hk::hook::writeBranchLinkAtMainOffset(0x4169C0, &makeSceneOwl);
+    //hk::hook::writeBranchLinkAtMainOffset(0x4169C0, &makeSceneOwl);
     // over deep sea
-    hk::hook::writeBranchLinkAtMainOffset(0x416DF4, &makeSceneSkater);
-
-    // dont wait to load scenes
-    hk::hook::writeBranchAtMainOffset(0x41f084, reinterpret_cast<void*>(mainBase + 0x41f0a8));
-
-    constexpr u64 remix20MainOffset = 0x417574;
-    hk::hook::a64::assemble<"mov x0, x20">().installAtMainOffset(remix20MainOffset);
-    hk::hook::a64::assemble<"mov x1, x19">().installAtMainOffset(remix20MainOffset + 4);
-    hk::hook::writeBranchLinkAtMainOffset(remix20MainOffset + 8, &remix20Main);
-    hk::hook::writeBranchAtMainOffset(remix20MainOffset + 12, reinterpret_cast<void*>(mainBase + 0x41799C));
-
-    constexpr u64 remix20ControlOffset = 0x41F198;
-    hk::hook::a64::assemble<"mov x0, x21">().installAtMainOffset(remix20ControlOffset);
-    hk::hook::a64::assemble<"mov x1, x19">().installAtMainOffset(remix20ControlOffset + 4);
-    hk::hook::writeBranchLinkAtMainOffset(remix20ControlOffset + 8, &remix20Control);
-    hk::hook::writeBranchAtMainOffset(remix20ControlOffset + 12, reinterpret_cast<void*>(mainBase + 0x42029C));
-
-    constexpr u64 remix20AnimOffset = 0x4179F0;
-    hk::hook::a64::assemble<"mov x0, x20">().installAtMainOffset(remix20AnimOffset);
-    hk::hook::a64::assemble<"mov x1, x19">().installAtMainOffset(remix20AnimOffset + 4);
-    hk::hook::writeBranchLinkAtMainOffset(remix20AnimOffset + 8, &remix20Anim);
-    hk::hook::writeBranchAtMainOffset(remix20AnimOffset + 12, reinterpret_cast<void*>(mainBase + 0x41800C));
-
-    remix20InitSubScenesHook.installAtMainOffset(0x41F050);
-    remix20Cues00Hook.installAtMainOffset(0x418AE0);
-    remix20Cues01Hook.installAtMainOffset(0x4194D0);
-    remix20Cues02Hook.installAtMainOffset(0x41A020);
-    remix20Cues03Hook.installAtMainOffset(0x41B8F0);
-    remix20Cues04Hook.installAtMainOffset(0x41DCE0);
-    remix20Cues05Hook.installAtMainOffset(0x41E770);
-    remix20Cues06Hook.installAtMainOffset(0x418020);
-    remix20Cues07Hook.installAtMainOffset(0x418500);
-*/
+    //hk::hook::writeBranchLinkAtMainOffset(0x416DF4, &makeSceneSkater);
 }
